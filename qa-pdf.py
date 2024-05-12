3765f9e1ab93b93dc1dfe4d8a665b333e60cf0c5
atakanG7 <pwxcv7352@gmail.com>
2024-05-12 07:30:34 +0300

    managing to be able to deploy to render

--- qa-pdf.py
+++ qa-pdf.py
@@ -12,13 +12,34 @@ import pickle
 from streamlit import chat_message
 import random
 
-PROMPT = PromptTemplate.from_template(template=""" 
+PROMPT = PromptTemplate.from_template(
+    template=""" 
     given context generate 5 questions out of given context never give your opinion. If the information is not enough to 
     generate 5 questions, you must say "please provide more information". You must add options like a, b, c and d as possible answers to each generated questions.
                         
     context = {context}
                         
-""")
+"""
+)
+
+# Initialize states
+if "chat_history" not in st.session_state:
+    st.session_state.key = None
+    st.session_state.chain = None
+    st.session_state.memory = None
+    st.session_state.text = None
+    st.session_state.pdfname = None
+    st.session_state.chat_history = []
+
+
+def download_file(file_path):
+    with open(file_path, "rb") as f:
+        st.download_button(
+            label="Download file",
+            data=f,
+            file_name=os.path.basename(file_path),
+            mime="application/pdf",
+        )
 
 
 def displayMessages(chat_history):
@@ -26,10 +47,10 @@ def displayMessages(chat_history):
     for message in reversed(chat_history):
         if count % 2 == 0:
             with chat_message("user"):
-                st.write(message.content)
+                st.write(message)
         else:
             with chat_message("assistant"):
-                st.write(message.content)
+                st.write(message)
         count += 1
 
 
@@ -40,44 +61,42 @@ def readMyPdf(pdf):
         st.session_state.text += page.extract_text()
 
 
-def extractMyEmbeddings():
-    text_splitter = CharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=300,
-        length_function=len
+def setConversation(temperature, question):
+    llm = ChatOpenAI(
+        temperature=temperature / 10,
+        model_name="gpt-3.5-turbo",
+        openai_api_key=st.session_state.key,
+    )
+    PROMPT = (
+        PromptTemplate.from_template(
+            template=f"""
+        I have the following document, please answer the question:
+        
+        question:
+        {question}
+        
+        document:
+        """
+        )
+        + st.session_state.text
     )
 
-    chunks = text_splitter.split_text(text=st.session_state.text)
-
-    embeddings = HuggingFaceInstructEmbeddings(
-        model_name="sentence-transformers/distiluse-base-multilingual-cased-v2")
-
-    return FAISS.from_texts(embedding=embeddings, texts=chunks)
+    st.session_state.chain = LLMChain(llm=llm, prompt=PROMPT)
 
 
-def setConversation(temperature):
-    llm = ChatOpenAI(temperature=temperature/10,
-                 model_name="gpt-3.5-turbo", openai_api_key=st.session_state.key)
-
-    memory = ConversationBufferMemory(
-        memory_key="chat_history", return_messages=True)
-
-    st.session_state.chain = ConversationalRetrievalChain.from_llm(
-        llm=llm, retriever=st.session_state.vectorStore.as_retriever(), memory=memory)
-
-
+# A function to let user download the existing pdf in the streamlit app
 def setConversation_1(temperature):
-    llm = ChatOpenAI(temperature=temperature/10,
-                 model_name="gpt-3.5-turbo", openai_api_key=st.session_state.key)
+    llm = ChatOpenAI(
+        temperature=temperature / 10,
+        model_name="gpt-3.5-turbo",
+        openai_api_key=st.session_state.key,
+    )
     st.session_state.chain1 = LLMChain(llm=llm, prompt=PROMPT)
 
 
 def retrieve5Chunks():
     text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=2000,
-        chunk_overlap=300,
-        length_function=len
+        separator="\n", chunk_size=2000, chunk_overlap=300, length_function=len
     )
     chunks = text_splitter.split_text(text=st.session_state.text)
     docs = text_splitter.create_documents(chunks)
@@ -87,57 +106,63 @@ def retrieve5Chunks():
     return random_docs
 
 
+# make questions look good using streamlit capabilities
+st.set_page_config(page_title="PDF Question-Answer", page_icon="📃", layout="wide")
+st.markdown(
+    """
+<style>
+    .reportview-container .main .block-container{.reportview-container .main {
+        min-height: 92vh;
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+# using streamlit capabilities, make this question and answer look good
+
+
 def main():
     st.header("Ask Questions To Your PDF 🗃️")
 
     with st.sidebar:
         st.image("./image/picture.png")
         st.header("Visit My Website")
-
+        download_file("./atakangul.pdf")
         st.text("https://www.atakangul.com")
 
         pdf = st.file_uploader(
-            label="Upload your pdf file below ", accept_multiple_files=False)
-
+            label="Upload your pdf file below ", accept_multiple_files=False
+        )
         st.session_state.key = st.text_input("OpenAI API Key", type="password")
 
-        temperature = st.slider(
-            "Temperature",
-            0, 10, 1)
+        temperature = st.slider("Temperature", 0, 10, 1)
 
     if pdf is not None:
         pdfName = pdf.name.split(".")[0]  # pdf name
 
-        if pdfName != st.session_state.get("pdfname", None):  # checking current pdf or not
+        if pdfName != st.session_state.get(
+            "pdfname", None
+        ):  # checking current pdf or not
             st.session_state["pdfname"] = pdfName
             st.session_state["text"] = ""
             readMyPdf(pdf)
-
-            if not os.path.exists(f"{pdfName}.pickle"):
-                st.info('file is being embedded, please wait! ', icon="ℹ️")
-                st.session_state["vectorStore"] = extractMyEmbeddings()
-                with open(f"{pdfName}.pickle", "wb") as file:
-                    pickle.dump(st.session_state.vectorStore, file=file)
-            else:
-                st.info('file is being loaded, please wait! ', icon="ℹ️")
-                with open(f"{pdfName}.pickle", "rb") as file:
-                    st.session_state["vectorStore"] = pickle.load(file=file)
-
-            setConversation(temperature)
 
         question = st.text_input("Ask to Pdf", key="input")  # user question
         check = st.checkbox("GenerateQPDF")  # check box
 
         if check and question:
-            st.info("You should choose one of the them cannot run at the same time!", icon="⚠️")
+            st.info(
+                "You should choose one of the them cannot run at the same time!",
+                icon="⚠️",
+            )
 
         elif question:
             st.write("---")
-
-            response = st.session_state.chain({'question': question})
-
-            chat_history = response["chat_history"]
-            displayMessages(chat_history)
+            setConversation(temperature, question)
+            response = st.session_state.chain({"question": question})
+            st.session_state.chat_history.append(response["question"])
+            st.session_state.chat_history.append(response["text"])
+            displayMessages(st.session_state.chat_history)
 
             st.write("---")
 
@@ -159,5 +184,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
